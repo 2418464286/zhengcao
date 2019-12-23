5bf19b93cf8fa0dcc83b7de6685198160b81fefd
2418464286 <18575037231zheng>
2019-12-23 19:48:22 +0800

    分支功能无敌

--- src/com/gg/gq/Fw.java
+++ src/com/gg/gq/Fw.java
@@ -6,6 +6,8 @@ public static void main(String[] args) {
 	System.out.println("强儿子");
 	System.out.println("儿强子");
 	System.out.println("强子儿");
+	System.out.println("儿狗强子");
+	System.out.println("狗强子儿");
 
 }
 }
